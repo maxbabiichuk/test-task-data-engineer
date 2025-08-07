54d8d53a62e5c5228e518d88b4b832ce9b320785
Maksym Babiichuk <maksym.babiichuk@proximaresearch.com>
2025-08-07 18:02:10 +0300

    refactor comments in books_schema.sql for clarity and correctness

--- books_schema.sql
+++ books_schema.sql
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS books_processed (
 -- 1. idx_books_genre - для швидкого пошуку книг за жанром
 CREATE INDEX IF NOT EXISTS idx_books_genre ON books (lower(genre));
 -- створив B-tree індекс він підійде для пошуку по чіткому співпадінню і для пошуку по префіксу.
--- додатково привів поле genre до нижнього регістру, як на мене, такий варіант більш оптимальний для простих задач пошуку.
--- загалом, пошук по текстовим полям - це досить шарока задача і її варто вирішувати згідно відповідних потреб
+-- додатково привів поле genre до нижнього регістру в індексі. Як на мене, такий варіант більш оптимальний для простих задач пошуку.
+-- загалом, пошук по текстовим полям - це досить широка задача і її варто вирішувати згідно відповідних потреб
 
 -- 2. idx_books_last_updated - для пошуку книг за датою останнього оновлення
 CREATE INDEX IF NOT EXISTS idx_books_last_updated ON books (last_updated);
@@ -33,7 +33,7 @@ CREATE INDEX IF NOT EXISTS idx_books_last_updated ON books (last_updated);
 CREATE INDEX IF NOT EXISTS idx_books_price_range ON books (price);
 -- B-tree індекс підходить для пошуку по діапазону
 
--- додавання рівно 6 записів в таблицю books
+-- додавання рівно 6-ти записів в таблицю books
 INSERT INTO books (title, price, genre, stock_quantity, last_updated) VALUES
 ('Назва книги 1', 299.99, 'фантастика', 15, '2025-01-15 10:30:00'),
 ('Назва книги 2', 435.87, 'детектив', 20, '2025-07-15 12:45:00'),
